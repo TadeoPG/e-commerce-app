344deb8a2d154ee5f48b569a040ae23cdfdbb880
TadeoPG <tadeomanuelportillo@gmail.com>
2024-09-22 01:29:10 -0500

    feat(config-server&discovery-client): add initial configuration for config server and discovery client

---
+++ services/discovery/src/test/java/com/tadeo/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tadeo.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
